0542a3c7a111294e4cbc38268cfc7660ff951b2f
Boyd Multerer <boyd@multerer.com>
2016-08-20 16:25:11 -0700

    try json operation

--- lib/ex_aws/transcoder.ex
+++ lib/ex_aws/transcoder.ex
@@ -8,7 +8,7 @@ defmodule ExAws.Transcoder do
   import ExAws.Utils, only: [camelize_keys: 1, upcase: 1]
   require Logger
 
-  @namespace "2012-09-25"
+  @namespace "/2012-09-25/"
 
   @type job_input :: map  
 
@@ -32,13 +32,15 @@ defmodule ExAws.Transcoder do
   ### Helper Functions ###
   ########################
 
-  defp request(http_method, path, data) do
-    %ExAws.Operation.RestQuery{
+  defp request(http_method, path, data, headers \\ [], before_request \\ nil) do
+    #path = [path, "?", params |> URI.encode_query] |> IO.iodata_to_binary
+    ExAws.Operation.JSON.new(:lambda, %{
       http_method: http_method,
-      path: path,
-      params: data,
-      service: :elastictranscoder
-    }
+      path: @namespace <> path,
+      data: data,
+      headers: [{"content-type", "application/json"} | headers],
+      before_request: before_request,
+    })
   end
 
   defp normalize_opts(opts) do
